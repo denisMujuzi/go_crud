49d02adca608eb7d98b177a0f2f7fdd607f46663
MUJUZI DENIS <2021bce014@std.must.ac.ug>
2025-11-16 14:24:15 +0300

    Refactor API structure: removed Vercel serverless handler, remove deprecated index.go, and implement main.go with routing for posts

--- api/main.go
+++ api/main.go
@@ -5,10 +5,7 @@ import (
 	"go_crud/api/initializers"
 	"go_crud/api/modules"
 
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	bridge "github.com/vercel/go-bridge/go/bridge"
 )
 
 func init() {
@@ -16,29 +13,26 @@ func init() {
 	initializers.ConnectToDB()
 }
 
-// This is required by Vercel, do not rename it
-func __NOW_HANDLER_FUNC_NAME(w http.ResponseWriter, r *http.Request) {
-	g := gin.New()
-	g.Use(gin.Logger(), gin.Recovery())
+func main() {
+	// Create a Gin router with default middleware (logger and recovery)
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
 
-	v1 := g.Group("/api/v1")
 	modules.RegisterUserRoutes(v1)
 
-	g.GET("/", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Welcome to the Go CRUD API",
 		})
+
 	})
 
+	// Define a simple GET endpoint
 	v1.POST("/posts", controllers.Postscreate)
 	v1.GET("/posts", controllers.PostsIndex)
 	v1.GET("/posts/:id", controllers.PostsShow)
 	v1.DELETE("/posts/:id", controllers.PostsDelete)
 	v1.PUT("/posts/:id", controllers.PostsUpdate)
 
-	g.ServeHTTP(w, r)
-}
-
-func main() {
-	bridge.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
+	r.Run()
 }
